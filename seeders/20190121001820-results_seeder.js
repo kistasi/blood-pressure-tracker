db53a7d55fe6e1007288fad874f76459fe1737bc
Marton Tasnadi <marton.tasnadi@gmail.com>
2019-01-21 01:35:55 +0100

    Add a seeder

---
+++ seeders/20190121001820-results_seeder.js
@@ -0,0 +1,39 @@
+const uuidV1 = require('uuid/v1');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'Results',
+      [
+        {
+          id: uuidV1(),
+          systolic: '435',
+          diastolic: '321',
+          pulse: '234',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: uuidV1(),
+          systolic: '122334',
+          diastolic: '3232',
+          pulse: '333',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: uuidV1(),
+          systolic: '1232334',
+          diastolic: '3241',
+          pulse: '444',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Result', null, {});
+  },
+};
